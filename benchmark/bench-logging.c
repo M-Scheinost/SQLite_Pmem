fc7cc19d41cbc30ca970db6719319896177ab174
Manuel Scheinsot <manuel.scheinost@tum.de>
2022-11-30 19:12:08 +0000

    insert a benchmark

--- benchmark/bench-logging.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "sqlite/sqlite3.h"
-
-int main(int argc, char** argv){
-
-}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "sqlite/sqlite3.h"
+#include "stdio.h"
+
+
+int main (int argc, char** argv){
+
+    sqlite3 *sqlite;
+}
