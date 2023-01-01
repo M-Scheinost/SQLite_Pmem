16379ce2e4fc39d6bf52b6173fd99ef7dcc03640
M-Scheinost <manuel.scheinost@tum.de>
2023-01-01 17:15:21 +0000

    script tweeks path setting is still wrong

--- benchmark/scripts/tatp.sh
+++ benchmark/scripts/tatp.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-for sf in 10000 100000 1000000; do
-  printf "*** TATP (scale factor %s) ***\n" "$sf"
-  path="benchmark.db"
-  printf "Loading data into SQLite3...\n"
-  ./tatp_bench --load --records=$sf --path=$path
 
-  printf "Evaluating SQLite3...\n"
-  #for journal_mode in "DELETE" "TRUNCATE" "WAL"; do
-    #for cache_size in "-100000" "-200000" "-500000" "-1000000" "-2000000" "-5000000"; do
-      
-      command="./tatp_bench --run --records=$sf --journal_mode=$journal_mode --cache_size=$cache_size --path=$path"
-      printf "%s\n" "$command"
-      for trial in {1..3}; do
-        printf "%s," "$trial"
-        eval "$command"
-      done
+for sf in 10000 100000 1000000; do
+  for pm in "true" "false"; do
+    printf "*** TATP (scale factor %s) ***\n" "$sf"
+
+    if [ $pm="true" ]
+    then
+      path="/mnt/pmem0/scheinost/benchmark.db"
+    else
+      path="benchmark.db"
+    fi
+
+    printf "Loading data into SQLite3...\n"
+    ./tatp_bench --load --records=$sf --path=$path --pmem=$pm
+
+    printf "Evaluating SQLite3...\n"
+    #for journal_mode in "DELETE" "TRUNCATE" "WAL"; do
+      #for cache_size in "-100000" "-200000" "-500000" "-1000000" "-2000000" "-5000000"; do
+
+        command="./tatp_bench --run --records=$sf --journal_mode=$journal_mode --cache_size=$cache_size --path=$path --pmem=$pm"
+        printf "%s\n" "$command"
+        for trial in {1..3}; do
+          printf "%s," "$trial"
+          eval "$command"
+        done
+      #done
     #done
-  #done
+  done
 
   rm $path
 
