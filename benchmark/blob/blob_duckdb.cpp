a99ef0e04cfab2fcc500923ef1c440cad24a4b27
M-Scheinost <manuel.scheinost@tum.de>
2023-01-13 13:40:45 +0000

    update setup

--- benchmark/blob/blob_duckdb.cpp
+++ benchmark/blob/blob_duckdb.cpp
@@ -1,7 +1,7 @@
 #include "cxxopts.hpp"
 #include "dbbench/runner.hpp"
 #include "helpers.hpp"
-#include "systems/duckdb/duckdb.hpp"
+#include "../../duckdb/duckdb.hpp"
 
 #include <atomic>
 #include <iostream>
--- duckdb/local.cmake
+++
@@ -1,5 +0,0 @@
-add_library(duckdb
-        ${CMAKE_SOURCE_DIR}/duckdb/duckdb.cpp
-        ${CMAKE_SOURCE_DIR}/duckdb/duckdb.hpp
-)
-target_link_libraries(duckdb Threads::Threads ${CMAKE_DL_LIBS})
