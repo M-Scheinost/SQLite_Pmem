e1b8ba0a4fe6f57d00ef18bf0b90a7ea25b07d21
M-Scheinost <manuel.scheinost@tum.de>
2023-02-07 21:47:49 +0000

    update2

--- benchmark/scripts/duckdb_tatp.sh
+++ benchmark/scripts/duckdb_tatp.sh
@@ -8,10 +8,10 @@ for sf in 10000 100000 1000000 10000000; do
   [ ! -e $path ] || rm $path*
 
   printf "Loading data into SQLite3 Pmem=$pm\n"
-  ./tatp_msc_dense --load --records=$sf --path=$path --pmem=$pm
+  ./tatp_msc_large --load --records=$sf --path=$path --pmem=$pm
 
   printf "Evaluating Pmem=$pm\n"
-    command="./tatp_msc_dense --run --records=$sf --path=$path --pmem=$pm" 
+    command="./tatp_msc_large --run --records=$sf --path=$path --pmem=$pm" 
     printf "%s\n" "$command"
     for trial in {1..3}; do
         [ ! -e $path-shm ] || rm $path-shm
